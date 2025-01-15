3508cef097ad945de977d0e39835de38345dcaa7
remanotft <remano@tailorfittech.com>
2025-01-16 01:07:03 +0200

    Added Sure Connect Widget

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,44 @@ import Aura from '@primevue/themes/aura';
 export default defineNuxtConfig({
 	compatibilityDate: '2024-11-01',
 	devtools: { enabled: true },
-	modules: ['@nuxtjs/tailwindcss', '@sidebase/nuxt-auth', '@primevue/nuxt-module'],
+	app: {
+		head: {
+			script: [
+				{
+					src: 'https://widget.mybookingplatform.com/widget.js?v=1.0',
+					type: 'text/javascript'
+				},
+				{
+					innerHTML: `var widget = new SSTWidget({
+							container: "SS-Widget",
+							SiteUrl: "https://zaptravelandtours.mybookingplatform.com/",
+							TabFontFamily: "Arial, Helvetica, sans-serif",
+							Disable: {
+								Hotel: false,
+								Tour: true,
+								Car: false,
+								Activity: true,
+								Cruise: true,
+								Flight : false,
+								Transfer : true
+							},
+							language:"en",
+							SearchButtonbackground: "#DEA042",
+							SearchButtonColor: "#fff"
+						});
+
+						widget.init();`,
+					tagPosition: 'bodyClose'
+				}
+			]
+		}
+	},
+	modules: [
+		'@nuxtjs/tailwindcss',
+		'@sidebase/nuxt-auth',
+		'@primevue/nuxt-module',
+		'@pinia/nuxt'
+	],
 	primevue: {
 		options: {
 			theme: {
--- pages/index.vue
+++ pages/index.vue
@@ -10,10 +10,40 @@
 		</ul>
 
 	</div>
+
+	<section class="relative overflow-hidden">
+		<div class="container relative w-full">
+			<div id="SS-Widget" class="SS-Widget md:h-[750px] lg:h-80">
+			</div>
+		</div>
+	</section>
 </template>
 
 <script lang="ts" setup>
 
 </script>
 
-<style></style>
+<style scoped>
+
+@media screen and (max-width: 600px) {
+	.SS-Widget {
+		width: 380px;
+		height: 780px;
+	}
+}
+
+@media screen and (max-width: 412px) {
+	.SS-Widget {
+		width: 365px;
+		height: 780px;
+	}
+}
+
+@media screen and (max-width: 375px) {
+	.SS-Widget {
+		width: 330px;
+		height: 780px;
+	}
+}
+
+</style>
